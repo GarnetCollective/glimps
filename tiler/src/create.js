68647976766978c93e0c94038307f999e4646242
Juan Alvarado <jalvarado91@gmail.com>
2018-07-15 17:07:56 -0400

    Add environemt vars

--- tiler/src/create.js
+++ tiler/src/create.js
@@ -4,6 +4,11 @@ import uuid from "uuid/v1";
 import { resolve as path_resolve } from "path";
 import { createCollage } from "./tiler";
 
+const {
+  AWS_ACCESS_KEY_ID: api_key,
+  AWS_SECRET_ACCESS_KEY: api_secret
+} = process.env;
+
 const brand = "fixtures/brand.jpg";
 
 const cleanUp = async images => {
@@ -30,25 +35,31 @@ const save = data => {
   });
 };
 
+const createStory = async storyImages => {
+  let images = [];
+  for (var shot of storyImages) {
+    let loc = await save(shot.base64);
+    images = images.concat([loc]);
+  }
+  console.log({ images });
+
+  let collage = await createCollage(images, brand);
+  console.log({ collage });
+  console.log("\n");
+
+  await cleanUp(images);
+
+  return collage;
+};
+
 const create = async (req, res) => {
   console.log(Date.now());
   try {
     let body = req.body;
-    let { story } = body;
+    let { story: storyImages } = body;
+    let story = await createStory(storyImages);
 
-    let images = [];
-    for (var shot of story) {
-      let loc = await save(shot.base64);
-      images = images.concat([loc]);
-    }
-    console.log({ images });
-
-    let collage = await createCollage(images, brand);
-    console.log({ collage });
-    console.log("\n");
-
-    await cleanUp(images);
-    res.status(200).json({ success: true, collage: collage.file_path });
+    res.status(200).json({ success: true, collage: story.file_path });
   } catch (e) {
     console.log(e);
     res.status(400).json({ success: false, error: e });
