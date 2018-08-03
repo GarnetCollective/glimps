20cd938969050050c026c1b6a2eab5a271975ef5
david-castaneda <dcast188@fiu.edu>
2018-08-03 14:17:39 -0400

    initiated glimps controller,model,services

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,6 @@ apiRouter.get("/events", events.index);
 apiRouter.get("/events/:id", events.show);
 apiRouter.post("/events", events.create);
 
-apiRouter.get("/glimps", glimps.index);
 apiRouter.get("/glimps/:id", glimps.show);
 
 export default apiRouter;
