88e7e6af0113f4287634df31770fabbc1e6b6e85
david-castaneda <dcast188@fiu.edu>
2018-08-02 20:12:31 -0400

    updated service && added validators/helpers

--- src/models/events.js
+++ src/models/events.js
@@ -1,30 +1,12 @@
 const EventModel = (sequelize, Sequelize) => {
   return sequelize.define("event", {
-    id: {
-      type: Sequelize.UUID,
-      primaryKey: true,
-      allowNull: false,
-      validate: {
-        isUUID: {
-          args: 4,
-          msg: "Please provide a valid uuid."
-        }
-      }
-    },
-    Name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: "Pleade provide a name."
-        }
-      }
-    },
-    Date: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW
-    }
+    id: { type: Sequelize.UUID, primaryKey: true, validate: { isUUID: 4 } },
+    name: { type: Sequelize.STRING, allowNull: false },
+    date: { type: Sequelize.DATE, allowNull: false },
+    mainImageUrl: { type: Sequelize.STRING },
+    logoUrl: { type: Sequelize.STRING },
+    secretKey: { type: Sequelize.STRING },
+    slug: { type: Sequelize.STRING }
   });
 };
 
