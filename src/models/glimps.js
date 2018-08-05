58dc23982879642abc97c2e80bcb8f974610bccf
Juan Alvarado <jalvarado91@gmail.com>
2018-08-05 14:22:09 -0400

    adds glimps creation api, controller, service

--- src/models/glimps.js
+++ src/models/glimps.js
@@ -1,7 +1,8 @@
+import EventModel from "./events";
+
 const GlimpsModel = (sequelize, Sequelize) => {
   return sequelize.define("glimps", {
     id: { type: Sequelize.UUID, primaryKey: true, validate: { isUUID: 4 } },
-    eventid: { type: Sequelize.STRING, validate: { isUUID: 4 } },
     imageUrl: { type: Sequelize.STRING },
     thumbUrl: { type: Sequelize.STRING }
   });
