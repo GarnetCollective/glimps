2d45fbaaa93e68c090913075e512fbd71606f2d5
david-castaneda <dcast188@fiu.edu>
2018-08-07 18:39:11 -0400

    added config setting for babel/jest

--- api/src/index.js
+++ api/src/index.js
@@ -12,11 +12,13 @@ import apiRoutes from "./routes";
 
 const app = express();
 
-sequelize
-  .sync()
-  // .sync({ force: true })
-  .then(() => console.log("> DB & Tables created."))
-  .catch(e => console.log(e.message));
+if (NODE_ENV !== "test") {
+  sequelize
+    .sync()
+    // .sync({ force: true })
+    .then(() => console.log("> DB & Tables created."))
+    .catch(e => console.log(e.message));
+}
 
 app.use(cors());
 app.use(helmet());
@@ -25,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
 
 app.use("/api", apiRoutes);
 
-if (NODE_ENV != "test") {
+if (NODE_ENV !== "test") {
   app.listen(PORT, err => {
     err && console.log(err.message);
     console.log(`> in ${NODE_ENV}`);
