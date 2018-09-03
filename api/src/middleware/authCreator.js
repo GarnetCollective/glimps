ecdf425df3a63bfa3af945272d146d3d65bc0009
Juan Alvarado <jalvarado91@gmail.com>
2018-09-03 19:38:07 -0400

    Conects to messaging service
    - Adds api,service,controller to send glimps over text using the courier service
    - Fixes issue with creator middleware
    - moves services urls to environment vars

--- api/src/middleware/authCreator.js
+++ api/src/middleware/authCreator.js
@@ -1,25 +1,22 @@
-import passport from "passport";
-import { Strategy as BearerStrategy } from "passport-http-bearer";
 import jwt from "jsonwebtoken";
 const { CREATOR_KEY, SECRET_KEY } = process.env;
 
-passport.use(
-  new BearerStrategy(async (token, done) => {
-    try {
-      let decoded = await jwt.verify(token, SECRET_KEY);
-      let creatorKey = decoded.creatorKey;
-      if (creatorKey !== CREATOR_KEY) {
-        return done(null, false);
-      }
-      return done(null, true);
-    } catch (err) {
-      return done(null, false);
-    }
-  })
-);
+const authCreator = async (req, res, next) => {
+  const { authorization } = req.headers;
 
-const authCreator = passport.authenticate("bearer", {
-  session: false
-});
+  if (!authorization) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  let token = authorization.split(" ")[1];
+
+  let decoded = await jwt.verify(token, SECRET_KEY);
+  let creatorKey = decoded.creatorKey;
+  if (creatorKey !== CREATOR_KEY) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  next();
+};
 
 export default authCreator;
