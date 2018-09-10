8f18ce0eb71165ca399f8f9e1c5fd31cdf1da064
Juan Alvarado <jalvarado91@gmail.com>
2018-09-09 20:16:29 -0400

    Fixes tiler host env var in api service

--- src/services/glimps.js
+++ src/services/glimps.js
@@ -1,10 +1,10 @@
 import { Glimps } from "../models";
 import eventService from "./events";
 import uuid from "uuid/v4";
-const { COURIER_HOST } = process.env;
+const { TILER_HOST } = process.env;
 
 import axios from "axios";
-const tiler = axios.create({ baseURL: COURIER_HOST });
+const tiler = axios.create({ baseURL: TILER_HOST });
 
 /**
  * @typedef {Object} Event
@@ -49,7 +49,8 @@ const findByEventId = eventId =>
   Glimps.findAll({
     where: {
       eventId: eventId
-    }
+    },
+    order: [["updatedAt", "DESC"]]
   });
 
 /**
