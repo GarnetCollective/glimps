290033aeceb98b80a7ab848dd20e191e999c8bc0
David Castaneda <dcast188@fiu.edu>
2018-09-22 17:17:19 -0400

    fixes issue #30 (#32)

--- src/middleware/authCreator.js
+++ src/middleware/authCreator.js
@@ -10,13 +10,17 @@ const authCreator = async (req, res, next) => {
 
   let token = authorization.split(" ")[1];
 
-  let decoded = await jwt.verify(token, SECRET_KEY);
-  let creatorKey = decoded.creatorKey;
-  if (creatorKey !== CREATOR_KEY) {
+  try {
+    let decoded = await jwt.verify(token, SECRET_KEY);
+    let creatorKey = decoded.creatorKey;
+    if (creatorKey !== CREATOR_KEY) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    next();
+  } catch (e) {
     return res.status(401).send("Unauthorized");
   }
-
-  next();
 };
 
 export default authCreator;
